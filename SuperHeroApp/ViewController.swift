4cb4357195e6d98b33c4e475744af89be7da5f75
Kryukov Andrey <65622566+KryukovAndrey@users.noreply.github.com>
2020-06-20 15:18:37 +0300

    Initial Commit

---
+++ SuperHeroApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperHeroApp
+//
+//  Created by Andrey on 20.06.2020.
+//  Copyright © 2020 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
